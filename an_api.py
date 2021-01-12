669612d05a056c500f9943613c0b014221d54733
Alan Mitchell <tabb99@gmail.com>
2021-01-11 21:42:03 -0900

    Added values file.

--- an_api.py
+++ an_api.py
@@ -42,16 +42,24 @@ def store_lora_data():
     """Store POST data into a cumulative LoRaWAN log file, and also into a file that
     holds the last post received.
     """
+    json_file = 'lora-data/lora.json'
+    gateway_file = 'lora-data/gateways.tsv'
+    values_file = 'lora-data/values.tsv'
+
     post_data = request.get_data(as_text=True)
     if not Path('lora-data/').exists():
         Path('lora-data/').mkdir()
 
-    open('lora-data/lora.json', 'a').write(f'{post_data}\n')
+    open(json_file, 'a').write(f'{post_data}\n')
 
     try:
-        if not Path('lora-data/gateways.tsv').exists():
+        if not Path(gateway_file).exists():
             hdr = 'dev_id\tts\tts_day\tts_hour\tcounter\tgateway\tsnr\trssi\tdata_rate\n'
-            with open('lora-data/gateways.tsv', 'w') as fout:
+            with open(gateway_file, 'w') as fout:
+                fout.write(hdr)
+        if not Path(values_file).exists():
+            hdr = 'dev_id\tts\tpayload\n'
+            with open(values_file, 'w') as fout:
                 fout.write(hdr)
         rec = json.loads(post_data)
         dev_id = rec['dev_id']
@@ -63,13 +71,16 @@ def store_lora_data():
         ts_day = ts.strftime('%Y-%m-%d')
         ts_hr = ts.strftime('%Y-%m-%d %H:00')
         data_rate = rec['metadata']['data_rate']
-        with open('lora-data/gateways.tsv', 'a') as fout:
+        with open(gateway_file, 'a') as fout:
             for gtw in rec['metadata']['gateways']:
                 gtw_id = gtw['gtw_id']
                 snr = gtw['snr']
                 rssi = gtw['rssi']
                 r = f'{dev_id}\t{ts_str}\t{ts_day}\t{ts_hr}\t{ctr}\t{gtw_id}\t{snr}\t{rssi}\t{data_rate}\n'
                 fout.write(r)
+        with open(values_file, 'a') as fout:
+            r = f"{dev_id}\t{ts_str}\t{rec['payload_raw']}\n"
+            fout.write(r)
     except:
         pass
 
